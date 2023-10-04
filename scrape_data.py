f3108dbbe8e8b2d461c998c37c66acbc73496589
Jacob Hiance <jakehiance@gmail.com>
2023-10-04 16:21:51 -0400

    Updated the data scraper and spotify API scripts

--- scrape_data.py
+++ scrape_data.py
@@ -29,42 +29,30 @@ if __name__ == "__main__":
     html_files = []
     for path in pathlib.Path(html_directory).iterdir():
         html_files.append(html_directory + "/" + path.name)
-    #print(html_files)
-    
-    #---------------------------------------------------------------------------
 
     songs = []
 
     for file in html_files:
 
-        #print("processing " + file + "...")
-
         html_file = open(file, "r")
         soup = BeautifulSoup(html_file.read(), "html5lib")
         html_file.close()
     
-        #decade_table = soup.find('table', attrs = {"bordercolor": "blue"})
         decade_table = soup.find("td", attrs = {"bgcolor": "#d6eaf8"})
         decade = decade_table.find_all("h3")
 
-        #print(len(decade))
         current_year = decade_table.find_next("h3")
         current_list = decade_table.find_next("ol")
 
-        #print(current_year)
-        #print(current_list)
-
         for i in range(10):
-            #print(current_year)
-            #print(current_list)
-            #print("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+
+            # Track the ranking of each song in each year's list
+            rank = 1
             
             for song in current_list.find_all("li"):
-                #print(song.text)
-                #print(current_year.text)
+
                 # Extract the song's year
                 song_year = current_year.text[:-1]
-                #print(song_year)
 
                 # Extract the part of the text containing the song's author
                 song_author = song.text.rsplit("\u201c", 1)[0].split(":")[1][1:]    # \u201c is a left double quote character that the web uses instead of standard "
@@ -72,28 +60,14 @@ if __name__ == "__main__":
                 # Extract the part of the text containing the song's title
                 song_title = song.text.rsplit("\u201c", 1)[1].split("\u201d")[0]    # \u201d is a right double quote character
 
-                #print(song_author + " --> " + song_title)
                 # Make sure to save the song so they can be analyzed later
-                songs.append((song_year, song_author, song_title))
+                songs.append((song_year, rank, song_author, song_title))
+
+                # The next song has a lower rank
+                rank += 1
             
             current_year = current_year.find_next("h3")
             current_list = current_list.find_next("ol")
-
-        """ for year in decade:
-            song_year = year.text[:-1]
-            song_list = decade_table.find_all_next("ol")
-            #print(song_list)
-            print("-----------------------------------------------------------------------------------------------------------------------------------------------------------")
-            for song in song_list.find_all("li"):
-                # Extract the part of the text containing the song's author
-                song_author = song.text.rsplit("\u201c", 1)[0].split(":")[1][1:]    # \u201c is a left double quote character that the web uses instead of standard "
-
-                # Extract the part of the text containing the song's title
-                song_title = song.text.rsplit("\u201c", 1)[1].split("\u201d")[0]    # \u201d is a right double quote character
-
-                #songs.append((song_year, song_author, song_title)) """
-                
-
 
     # If the specified save file does not exist, we should just create it
     with open(sys.argv[2], "w") as file:
--- spotify_api.py
+++ spotify_api.py
@@ -5,6 +5,7 @@ from requests import get, post
 import json
 
 def get_token(client_id, client_secret):
+
     authorization_string = client_id + ":" + client_secret
     authorization_bytes = authorization_string.encode("utf-8")
     authorization_base64 = str(base64.b64encode(authorization_bytes), "utf-8")
@@ -17,44 +18,13 @@ def get_token(client_id, client_secret):
     data = {"grant_type": "client_credentials"}
 
     result = post(url, headers=headers, data = data)
-
     json_result = json.loads(result.content)
-
     token = json_result["access_token"]
 
     return token
 
 def get_authorization_header(token):
     return {"Authorization": "Bearer " + token}
-
-def search_for_artist(token, artist_name):
-
-    url = "https://api.spotify.com/v1/search"
-    headers = get_authorization_header(token)
-    query = f"q={artist_name}&type=artist&limit=1"
-
-    query_url = url + "?" + query
-
-    result = get(query_url, headers = headers)
-    json_result = json.loads(result.content)["artists"]["items"]
-
-    if len(json_result) == 0:
-        print("Artist not found")
-        return None
-    
-    return json_result[0]
-
-    #print(json.dumps(json_result, indent = 4))
-
-def get_top_tracks(token, artist_id):
-
-    url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
-    headers = get_authorization_header(token)
-    result = get(url, headers = headers)
-
-    json_result = json.loads(result.content)["tracks"]
-
-    return json_result
 
 def get_song_id(token, artist, track):
 
@@ -68,7 +38,17 @@ def get_song_id(token, artist, track):
     result = get(search_url, headers = headers)
     json_result = json.loads(result.content)["tracks"]["items"][0]["id"]
 
-    return json.dumps(json_result, indent = 4)
+    return json_result
+
+def get_track_features(token, track_id):
+
+    url = f"https://api.spotify.com/v1/audio-features/{track_id}"
+    headers = get_authorization_header(token)
+
+    result = get(url, headers = headers)
+    json_result = json.loads(result.content)
+
+    print(json.dumps(json_result, indent = 4))
 
 if __name__ == "__main__":
 
@@ -78,12 +58,6 @@ if __name__ == "__main__":
     client_secret = os.getenv("CLIENT_SECRET")
 
     token = get_token(client_id, client_secret)
-    result = search_for_artist(token, "ACDC")
-    artist_id = result["id"]
-    songs = get_top_tracks(token, artist_id)
-    
-    #for index, song in enumerate(songs):
-        #print(f"{index + 1}.) {song['name']}")
-    
-    song_name = get_song_id(token, "AC/DC", "Highway%20to%20Hell")
-    print(song_name[1:-1])
+
+    song_id = get_song_id(token, "AC/DC", "Highway to Hell")
+    get_track_features(token, song_id)
